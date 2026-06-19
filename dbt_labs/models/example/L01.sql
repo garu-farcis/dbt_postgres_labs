d727d34b6bae3c6e325e696d7b55066494c8458d
garu-farcis <priyankagaru2024@gmail.com>
2026-06-19 15:22:56 +0200

    dbt postgres model practice

---
+++ dbt_labs/models/example/L01.sql
@@ -0,0 +1,24 @@
+CREATE TYPE phone_number AS (
+area_code varchar(3),
+line_number varchar(7)
+CREATE OR REPLACE FUNCTION phone_number_equal (phone_
+number,phone_number) RETURNS boolean AS
+);
+$$
+BEGIN
+IF $1.area_code=$2.area_code AND
+$1.line_number=$2.line_number THEN
+RETURN TRUE ;
+ELSE
+END IF;
+END;
+$$
+LANGUAGE plsql;
+CREATE OPERATOR = (
+LEFTWARD = phone_number,
+RIGHTWARD = phone_number,
+PROCEDURE = phone_number_equal
+);
+--For test purpose
+SELECT row('123','222244')::phone_number =
+row('1','222244')::phone_number;
---
+++ dbt_labs/models/example/my_first_model.sql
@@ -0,0 +1,5 @@
+-- models/example/first_model.sql
+SELECT
+    1 AS id,
+    'Hello dbt!' AS message,
+    CURRENT_DATE AS run_date
